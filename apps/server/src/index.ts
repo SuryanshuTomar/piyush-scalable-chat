de1c0717c18c037858eb299f4e3824ecf3e31cbb
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-31 16:13:03 +0530

    add: logic to initialize listeners

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,13 +8,15 @@ dotenv.config({ path: "./.env" });
 async function init() {
 	const PORT = (process.env.PORT ?? 8000) as number;
 	const httpServer = http.createServer();
-  
+
 	const socketService = new SocketService();
 	socketService.io.attach(httpServer);
 
 	httpServer.listen(PORT, () => {
 		console.log(`HTTP server started at PORT: ${PORT}`);
 	});
+
+	socketService.initializeListeners();
 }
 
 init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,11 +1,25 @@
 import { Server } from "socket.io";
 
-
 export class SocketService {
 	private _io: Server;
 	constructor() {
 		console.log("Socker Server Initialized...");
 		this._io = new Server();
+	}
+
+	public initializeListeners() {
+		console.log("Socket Listeners Initialized!");
+		const io = this._io;
+		io.on("connect", (socket) => {
+			console.log("New Socket Connected : ", socket.id);
+
+			socket.on(
+				"event:message",
+				async ({ message }: { message: string }) => {
+					console.log("New Message Received : ", message);
+				}
+			);
+		});
 	}
 
 	get io() {
