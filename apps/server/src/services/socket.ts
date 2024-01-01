1b80db035561f94e80333cc5bf8099fc7808119c
Suryanshu Tomar <suryanshutomar123@outlook.com>
2024-01-01 14:31:41 +0530

    refactor: add cors options

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ export class SocketService {
 	private _io: Server;
 	constructor() {
 		console.log("Socker Server Initialized...");
-		this._io = new Server();
+		this._io = new Server({
+			cors: {
+				allowedHeaders: ["*"],
+				origin: "http://localhost:3000",
+			},
+		});
 	}
 
 	public initializeListeners() {
